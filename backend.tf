53171edfd872b6bb48342c7711ccd9ce5fead35b
Akbar335 <62642055+Akbar335@users.noreply.github.com>
2025-04-27 17:25:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "Terraform"
-    storage_account_name = "demostorageacc99099"
-    container_name       = "democn09"
+    storage_account_name = "terraform_demostorageacc99099"
+    container_name       = "terraform_democn09"
     key                  = "default.terraform.tfstate"
   }
 }
