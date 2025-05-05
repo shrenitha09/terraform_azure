fc25250734791faf7e79ce49a21c5f17bfb7a95c
Akbar335 <62642055+Akbar335@users.noreply.github.com>
2025-05-05 20:04:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,5 +7,13 @@ variable "location" {
 variable "env" { }
 variable "public-subnet" { }
 variable "private-subnet" { }
-
+variable "subscription_id" {}
+variable "client_id" {}
+variable "object_id" {}
+variable "client_secret" {
+  description = "client secret for authentication"
+  type        = string
+  sensitive   = true
+}
+variable "tenant_id" {}
 
